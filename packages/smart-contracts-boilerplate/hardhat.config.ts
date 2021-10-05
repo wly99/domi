615a00fd9feac04a78f0a1bd09450a25c6335369
wly99 <34613421+wly99@users.noreply.github.com>
2021-10-05 13:19:50 +0800

    build: last of setup

--- packages/smart-contracts-boilerplate/hardhat.config.ts
+++ packages/smart-contracts-boilerplate/hardhat.config.ts
@@ -1,10 +1,15 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 import { task, HardhatUserConfig } from 'hardhat/config';
 import '@nomiclabs/hardhat-waffle';
-import "solidity-coverage";
-import "hardhat-gas-reporter";
-import "hardhat-contract-sizer";
+import 'solidity-coverage';
+import 'hardhat-gas-reporter';
+import 'hardhat-contract-sizer';
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import { removeConsoleLog } from "hardhat-preprocessor";
+import "hardhat-deploy";
+import { nodeUrl, accounts } from "./utils/network";
 
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -20,6 +25,7 @@ const config: HardhatUserConfig = {
     tests: './test',
     artifacts: './build/artifacts',
     cache: './build/cache',
+    deploy: "./scripts/deploy",
   },
   solidity: {
     compilers: [
@@ -38,7 +44,7 @@ const config: HardhatUserConfig = {
     timeout: 20000,
   },
   gasReporter: {
-    currency: "USD",
+    currency: 'USD',
     enabled: process.env.REPORT_GAS ? true : false,
     showMethodSig: true,
     onlyCalledMethods: false,
@@ -49,6 +55,36 @@ const config: HardhatUserConfig = {
     disambiguatePaths: false,
     runOnCompile: true,
     strict: true,
+  },
+  typechain: {
+    outDir: "typechained",
+    target: "ethers-v5",
+  },
+  preprocess: {
+    eachLine: removeConsoleLog(
+      (hre) =>
+        hre.network.name !== "hardhat" && hre.network.name !== "localhost"
+    ),
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337, // temporary for MetaMask support: https://github.com/MetaMask/metamask-extension/issues/10290
+    },
+    localhost: {
+      url: nodeUrl("localhost"),
+    },
+    rinkeby: {
+      url: nodeUrl("rinkeby"),
+      accounts: accounts("rinkeby"),
+    },
+    // can configure other networks, see examples:
+    // https://github.com/wighawag/template-ethereum-contracts/blob/main/hardhat.config.ts
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // by default, take the first account as deployer
+      rinkeby: "0x428C7F55cF935909512d845e3740b1C81CB38632", // on rinkeby, use your account
+    },
   },
 };
 
