ffbb739ab2ce3686956eb8b5df7133215235f49f
BRANDON <brundon.cyx@gmail.com>
2021-10-05 16:05:21 +0800

    feat: updated typechain

--- packages/smart-contracts-boilerplate/typechained/Greeter.d.ts
+++ packages/smart-contracts-boilerplate/typechained/Greeter.d.ts
@@ -13,26 +13,29 @@ import {
   ContractTransaction,
   Overrides,
   CallOverrides,
-} from 'ethers';
-import { BytesLike } from '@ethersproject/bytes';
-import { Listener, Provider } from '@ethersproject/providers';
-import { FunctionFragment, EventFragment, Result } from '@ethersproject/abi';
-import type { TypedEventFilter, TypedEvent, TypedListener } from './common';
+} from "ethers";
+import { BytesLike } from "@ethersproject/bytes";
+import { Listener, Provider } from "@ethersproject/providers";
+import { FunctionFragment, EventFragment, Result } from "@ethersproject/abi";
+import type { TypedEventFilter, TypedEvent, TypedListener } from "./common";
 
 interface GreeterInterface extends ethers.utils.Interface {
   functions: {
-    'greet()': FunctionFragment;
-    'greeting()': FunctionFragment;
-    'setGreeting(string)': FunctionFragment;
+    "greet()": FunctionFragment;
+    "greeting()": FunctionFragment;
+    "setGreeting(string)": FunctionFragment;
   };
 
-  encodeFunctionData(functionFragment: 'greet', values?: undefined): string;
-  encodeFunctionData(functionFragment: 'greeting', values?: undefined): string;
-  encodeFunctionData(functionFragment: 'setGreeting', values: [string]): string;
+  encodeFunctionData(functionFragment: "greet", values?: undefined): string;
+  encodeFunctionData(functionFragment: "greeting", values?: undefined): string;
+  encodeFunctionData(functionFragment: "setGreeting", values: [string]): string;
 
-  decodeFunctionResult(functionFragment: 'greet', data: BytesLike): Result;
-  decodeFunctionResult(functionFragment: 'greeting', data: BytesLike): Result;
-  decodeFunctionResult(functionFragment: 'setGreeting', data: BytesLike): Result;
+  decodeFunctionResult(functionFragment: "greet", data: BytesLike): Result;
+  decodeFunctionResult(functionFragment: "greeting", data: BytesLike): Result;
+  decodeFunctionResult(
+    functionFragment: "setGreeting",
+    data: BytesLike
+  ): Result;
 
   events: {};
 }
@@ -61,7 +64,9 @@ export class Greeter extends BaseContract {
     eventFilter: TypedEventFilter<EventArgsArray, EventArgsObject>,
     listener: TypedListener<EventArgsArray, EventArgsObject>
   ): this;
-  removeAllListeners<EventArgsArray extends Array<any>, EventArgsObject>(eventFilter: TypedEventFilter<EventArgsArray, EventArgsObject>): this;
+  removeAllListeners<EventArgsArray extends Array<any>, EventArgsObject>(
+    eventFilter: TypedEventFilter<EventArgsArray, EventArgsObject>
+  ): this;
 
   listeners(eventName?: string): Array<Listener>;
   off(eventName: string, listener: Listener): this;
@@ -83,14 +88,20 @@ export class Greeter extends BaseContract {
 
     greeting(overrides?: CallOverrides): Promise<[string]>;
 
-    setGreeting(_greeting: string, overrides?: Overrides & { from?: string | Promise<string> }): Promise<ContractTransaction>;
+    setGreeting(
+      _greeting: string,
+      overrides?: Overrides & { from?: string | Promise<string> }
+    ): Promise<ContractTransaction>;
   };
 
   greet(overrides?: CallOverrides): Promise<string>;
 
   greeting(overrides?: CallOverrides): Promise<string>;
 
-  setGreeting(_greeting: string, overrides?: Overrides & { from?: string | Promise<string> }): Promise<ContractTransaction>;
+  setGreeting(
+    _greeting: string,
+    overrides?: Overrides & { from?: string | Promise<string> }
+  ): Promise<ContractTransaction>;
 
   callStatic: {
     greet(overrides?: CallOverrides): Promise<string>;
@@ -107,7 +118,10 @@ export class Greeter extends BaseContract {
 
     greeting(overrides?: CallOverrides): Promise<BigNumber>;
 
-    setGreeting(_greeting: string, overrides?: Overrides & { from?: string | Promise<string> }): Promise<BigNumber>;
+    setGreeting(
+      _greeting: string,
+      overrides?: Overrides & { from?: string | Promise<string> }
+    ): Promise<BigNumber>;
   };
 
   populateTransaction: {
@@ -115,6 +129,9 @@ export class Greeter extends BaseContract {
 
     greeting(overrides?: CallOverrides): Promise<PopulatedTransaction>;
 
-    setGreeting(_greeting: string, overrides?: Overrides & { from?: string | Promise<string> }): Promise<PopulatedTransaction>;
+    setGreeting(
+      _greeting: string,
+      overrides?: Overrides & { from?: string | Promise<string> }
+    ): Promise<PopulatedTransaction>;
   };
 }
