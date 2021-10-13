d82019603a6af829b54cf58726885a42fa8508ed
wly99 <34613421+wly99@users.noreply.github.com>
2021-10-13 18:57:45 +0800

    refactor: prettier

--- packages/smart-contracts-boilerplate/contracts/Distributor.sol
+++ packages/smart-contracts-boilerplate/contracts/Distributor.sol
@@ -4,24 +4,25 @@ import './Ownable.sol';
 
 abstract contract DomiInterface {
   function getStabilityFee() public view virtual returns (uint256 stabilityFee);
-  function getTotalSupply() public view virtual returns (uint totalSupply);
-  function balanceOf(address whom) public view virtual returns (uint);
+
+  function getTotalSupply() public view virtual returns (uint256 totalSupply);
+
+  function balanceOf(address whom) public view virtual returns (uint256);
 }
 
 contract Distributor is Ownable {
-    DomiInterface domiContract;
+  DomiInterface domiContract;
 
-    function setDomiContractAddress(address _address) external onlyOwner {
-        domiContract = DomiInterface(_address);
-    }
+  function setDomiContractAddress(address _address) external onlyOwner {
+    domiContract = DomiInterface(_address);
+  }
 
-    function isSufficient() external view returns (bool) {
-        uint stabilityFee = domiContract.getStabilityFee();
-        uint totalStabilityFeesOwed = domiContract.getTotalSupply() * stabilityFee;
-        if (totalStabilityFeesOwed <= domiContract.balanceOf(address(this))) {
-            return true;
-        }
-        return false;
+  function isSufficient() external view returns (bool) {
+    uint256 stabilityFee = domiContract.getStabilityFee();
+    uint256 totalStabilityFeesOwed = domiContract.getTotalSupply() * stabilityFee;
+    if (totalStabilityFeesOwed <= domiContract.balanceOf(address(this))) {
+      return true;
     }
-    
+    return false;
+  }
 }
