0139e15e1de10c7ec922313f53ee2b18ec517084
BRANDON <brundon.cyx@gmail.com>
2021-10-15 14:51:08 +0800

    feat(Home): added HomePriceCalculator and tests

---
+++ packages/smart-contracts-boilerplate/contracts/HomePriceCalculator.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HomePriceCalculator {
+    
+    event HomePriceReceived(uint housePrice);
+
+    function determineHomePrice(uint postalCode, string memory streetName) public returns (uint) {
+        uint housePrice = 10000;
+        if (compareStrings(streetName, "Orchard")) { housePrice += 10000; }
+        if (postalCode % 2 == 0) { housePrice += 1000; } // if even
+        emit HomePriceReceived(housePrice);
+        return housePrice; 
+    }
+    
+    function compareStrings(string memory a, string memory b) public view returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+}
---
+++ packages/smart-contracts-boilerplate/test/HomePriceCalculator.test.ts
@@ -0,0 +1,29 @@
+import chai, { expect } from 'chai';
+import { ethers, waffle } from 'hardhat';
+import { HomePriceCalculator, HomePriceCalculator__factory } from '@typechained';
+const { solidity } = waffle;
+
+chai.use(solidity);
+
+describe('HomePriceCalculator', () => {
+  let homePriceCalculator: HomePriceCalculator;
+  let homePriceCalFactory: HomePriceCalculator__factory;
+
+  beforeEach(async () => {
+    homePriceCalFactory = (await ethers.getContractFactory('HomePriceCalculator')) as HomePriceCalculator__factory;
+    homePriceCalculator = await homePriceCalFactory.deploy();
+  });
+
+  it('Should return 21000 when postal code is even and location is Orchard', async () => {
+    expect(await homePriceCalculator.determineHomePrice(510212, "Orchard")).to.equal(21000);
+  });
+
+  it('Should return 20000 when postal code is odd and location is Orchard', async () => {
+    expect(await homePriceCalculator.determineHomePrice(510211, "Orchard")).to.equal(20000);
+  });
+
+  it('Should return 11000 when postal code is even and location is not Orchard', async () => {
+    expect(await homePriceCalculator.determineHomePrice(510212, "Bishan")).to.equal(11000);
+  });
+
+});
