d628f82ca811d0084a08808c5d4bc59ae4450844
wly99 <34613421+wly99@users.noreply.github.com>
2021-10-21 09:53:48 +0800

    refactor: move distributor function into domi

--- packages/smart-contracts-boilerplate/contracts/Domi.sol
+++ packages/smart-contracts-boilerplate/contracts/Domi.sol
@@ -1,15 +1,18 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import './Ownable.sol';
 
-contract DomiToken is ERC20 {
-    address private owner;
-    address private recipient;
-    uint stabilityFee;
+contract DomiToken is ERC20, Ownable {
+    address private _owner;
+    address private _recipient;
+    uint public stabilityFee;
+    uint256 public lastDistributed;
     
-    constructor() public ERC20("DomiToken", "Domi") {
+    constructor() public ERC20('DomiToken', 'Domi') {
         _mint(msg.sender, 100 * (10 ** uint256(decimals())));
         owner = msg.sender;
+        lastDistributed = block.timestamp;
     }
 
     function transferTokens(msg.sender, recipient,  uint amount) external {
@@ -19,5 +22,27 @@ contract DomiToken is ERC20 {
     function getTokenOwner() public view returns (address){ 
         return owner;
     }
+
+    function isSufficient() external view returns (bool) {
+        uint256 totalStabilityFeesOwed = _totalSupply * stabilityFee;
+        if (totalStabilityFeesOwed <= _balances[address(this)]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function distribute() external onlyOwner {
+    require(block.timestamp - 28 days >= lastDistributed, 'Wait at least 28 days');
+    uint256 totalStabilityFeesOwed = _totalSupply * stabilityFee;
+    require(
+      totalStabilityFeesOwed <= domiContract.balanceOf(address(this)),
+      'Insufficient stability fees'
+    );
+    // uint256 numDomiHolders = domiHolders.length;
+    // for (uint256 i = 0; i < numDomiHolders; i++) {
+    //   // TODO transfer(address(this), domiHolders[i], balances[domiHolders[i]] / totalSupply * totalStabilityFeesOwed);
+    // }
+  }
 
 }
