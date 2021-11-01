8709e89d20cde7761bc2f3944ddcbfe5ed5e9ff5
snehaarajkmrr <75203723+snehaarajkmrr@users.noreply.github.com>
2021-11-01 16:31:40 +0800

    feat: add Reserves.sol

--- packages/smart-contracts-boilerplate/contracts/Reserves.sol
+++ packages/smart-contracts-boilerplate/contracts/Reserves.sol
@@ -2,53 +2,58 @@
 pragma solidity ^0.8.0;
 
 import './Ownable.sol';
-import './Homes.sol';
 import './Reserves.sol';
 import './HomePriceCalculator.sol';
-import './Domi.sol';
-import './Collector.sol';
+import "hardhat/console.sol";
+
+abstract contract DomiInterface {
+  function transferTokens(
+    address sender,
+    address recipient,
+    uint256 amount
+  ) external payable virtual;
+}
 
 contract Reserves{
+    DomiInterface public domiContract;
+    mapping(address => uint256) public reservesContractAddress;
+
+    constructor() {
+        console.log("Deployed Reserves by '%s'", msg.sender);
+    }
     address ReservesContract;
     uint totalReserves;
-    function addtoReserves(address collector, uint amount) returns (bool){
-        bool added;
-        try{
-            Domi.transferTokens(address collector, address ReservesContract, uint amount)
-            added=True;
-        }
-        catch{
-            added=False;
-        }
-        return added
+    function addtoReserves(address collector, uint amount) public {
+        bool success;
+        success = true;
+        domiContract.transferTokens(collector, msg.sender, amount);
     }
-    function withdrawfromReserves(address distributor) returns (bool){
+    function withdrawfromReserves(address distributor, uint amount) public returns (bool){
           bool withdraw;
-        try{
-            Domi.transferTokens(address ReservesContract, address distributor, uint amount)
-            withdraw=True;
+        try  domiContract.transferTokens(msg.sender, distributor, amount){
+            withdraw=true;
         }
         catch{
-            withdraw=False;
+            withdraw=false;
         }
         return withdraw;
     }
 
-    function transferToBuyHomes(address buyHomes) returns (bool){
-          bool transfered;
-          threshold=6*getStabilityFee();
-        if totalReserves>threshold{
-            try{
-                Domi.transferTokens(address ReservesContract, address distributor, uint amount)
-                transfered=True;
+    function transferToBuyHomes(address buyHomes, uint amount) public returns (bool){
+          bool transferred;
+          uint threshold=10000;
+
+        if (reservesContractAddress[msg.sender]>threshold){
+            try domiContract.transferTokens(buyHomes, msg.sender, amount){
+                transferred=true;
             }
             catch{
-                transfered=False;
+                transferred=false;
             }
         }
         else{
-            transfered=False;
+            transferred=false;
         }
-        return transfered;
+        return transferred;
     }
 }
