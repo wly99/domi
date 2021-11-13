411e73d8a1f54e9faff891c0e4264eeb1b9a8e93
BRANDON <brundon.cyx@gmail.com>
2021-11-14 00:38:21 +0800

    refactor: updated function

--- packages/smart-contracts-boilerplate/contracts/DomiMockToken.sol
+++ packages/smart-contracts-boilerplate/contracts/DomiMockToken.sol
@@ -1,21 +1,20 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract DomiMockToken is ERC20 {
-    address private _owner;
-    uint256 public lastDistributed;
-    uint256 public numOfHolders;
-    mapping(address => bool) private _addressInitialized;
-    address[] private _domiHolders;
+  address private _owner;
+  uint256 public lastDistributed;
+  uint256 public numOfHolders;
+  mapping(address => bool) private _addressInitialized;
+  address[] private _domiHolders;
 
-
-    constructor() ERC20('DomiToken', 'Domi') {
-      uint256 initialSupply = 100;
-      _mint(msg.sender, initialSupply);
-      _owner = msg.sender;
-      lastDistributed = block.timestamp;
-      numOfHolders = 0;
-    }
+  constructor() ERC20('DomiToken', 'Domi') {
+    uint256 initialSupply = 100;
+    _mint(msg.sender, initialSupply);
+    _owner = msg.sender;
+    lastDistributed = block.timestamp;
+    numOfHolders = 0;
+  }
 }
